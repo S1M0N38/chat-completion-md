ccf3d910ae5d28dab6251260bcd55b7b4f6d2d1e
S1M0N38 <bertolottosimone@gmail.com>
2024-12-28 11:18:07 +0100

    test(json_to_md): add test for example and invalid requests

--- tests/test_json_to_md.py
+++ tests/test_json_to_md.py
@@ -1,16 +1,51 @@
-import json
-import pytest
+from json import JSONDecodeError
 from pathlib import Path
+
+import pytest
+from pydantic import ValidationError
+
 from chat_completion_md import json_to_md
 
 here = Path(__file__).parent
-json_flies: list[Path] = sorted((here / "data" / "json_to_md").glob("*.json"))
-md_flies: list[Path] = sorted((here / "data" / "json_to_md").glob("*.md"))
+
+class TestJsonToMd:
+
+    data_path = here / "data" / "json_to_md"
+    example_jsons: list[Path] = sorted(data_path.glob("example_*.json"))
+    example_mds: list[Path] = sorted(data_path.glob("example_*.md"))
 
 
-@pytest.mark.parametrize("json_file, md_file", zip(json_flies, md_flies))
-def test_json_to_md(json_file, md_file):
-    json_str = json.loads(json_file.read_text())
-    md_str = md_file.read_text()
-    output = json_to_md(json_str)
-    assert output == md_str
+    @pytest.mark.parametrize("json_file, md_file", zip(example_jsons, example_mds))
+    def test_examples(self, json_file, md_file):
+        json_str = json_file.read_text()
+        md_str = md_file.read_text()
+        output = json_to_md(json_str)
+        assert output == md_str
+
+    def test_not_valid_json(self):
+        json_str = (self.data_path / "not_valid_json.json").read_text()
+        with pytest.raises(JSONDecodeError) as e:
+            json_to_md(json_str)
+            print(str(e))
+
+    def test_missing_messages(self):
+        json_str = (self.data_path / "missing_messages.json").read_text()
+        with pytest.raises(KeyError) as e:
+            json_to_md(json_str)
+            assert "messages key not found in JSON" in str(e)
+
+    def test_missing_model(self):
+        json_str = (self.data_path / "missing_model.json").read_text()
+        with pytest.raises(ValidationError) as exc_info:
+            json_to_md(json_str)
+        errors = exc_info.value.errors()[0]
+        assert "model" in errors["loc"]
+        assert "Field required" == errors["msg"]
+
+    def test_wrong_role(self):
+        json_str = (self.data_path / "wrong_role.json").read_text()
+        with pytest.raises(ValidationError) as exc_info:
+            json_to_md(json_str)
+        errors = exc_info.value.errors()[0]
+        msg = "Input should be 'system', 'user', 'assistant', 'developer' or 'tool'"
+        assert msg == errors["msg"]
